dd87624a4f171701cb5ec31e3b73504d7afba112
炫炫 <2066855608@qq.com>
2025-05-22 04:37:14 +0800

    1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -39,7 +39,10 @@ export default defineUserConfig({
         Components({
           resolvers: [ElementPlusResolver()],
         }),
-      ]
+      ],
+      ssr: {
+        noExternal: [/element-plus/]
+      },
     }
   }),
   shouldPrefetch: false, // 站点较大，页面数量较多时，不建议启用
