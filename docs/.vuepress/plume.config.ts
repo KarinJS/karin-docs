345ec1ad51d1a7e3454e70a511ab265138a5e8a4
MapleLeaf <197728340@qq.com>
2025-05-28 09:37:56 +0800

    修正社交链接中的 GitHub URL

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -11,7 +11,7 @@ export default defineThemeConfig({
   appearance: true,  // 配置 深色模式
 
   social: [
-    { icon: 'github', link: '/' },
+    { icon: 'github', link: 'https://github.com/KarinJS/Karin' },
   ],
   // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
